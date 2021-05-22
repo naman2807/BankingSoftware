aadfd13ca6231470d4af9af78ec99c5e83dd2ba3
naman2807 <namanagarwaldbs438@gmail.com>
2021-05-22 12:10:12 +0530

    Added getter and setter of Bank.java class

--- src/data/Bank.java
+++ src/data/Bank.java
@@ -14,6 +14,44 @@ import javafx.collections.ObservableList;
 
 public class Bank {
     private SimpleStringProperty name = new SimpleStringProperty("");
-    private ObservableList<Branch> branches = FXCollections.observableArrayList();
+    private ObservableList<Branch> branches;
     private SimpleStringProperty bankCode = new SimpleStringProperty("");
+
+    public Bank(String name) {
+        this.name.set(name);
+        this.branches = FXCollections.observableArrayList();
+        this.bankCode = bankCode;
+    }
+
+    public String getName() {
+        return name.get();
+    }
+
+    public SimpleStringProperty nameProperty() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name.set(name);
+    }
+
+    public ObservableList<Branch> getBranches() {
+        return branches;
+    }
+
+    public void setBranches(ObservableList<Branch> branches) {
+        this.branches = branches;
+    }
+
+    public String getBankCode() {
+        return bankCode.get();
+    }
+
+    public SimpleStringProperty bankCodeProperty() {
+        return bankCode;
+    }
+
+    public void setBankCode(String bankCode) {
+        this.bankCode.set(bankCode);
+    }
 }
