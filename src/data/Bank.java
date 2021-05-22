82dd8db289ecddce9aa8777aafdbcc672f110218
naman2807 <namanagarwaldbs438@gmail.com>
2021-05-22 12:05:53 +0530

    Created a new class called Bank.java

---
+++ src/data/Bank.java
@@ -0,0 +1,12 @@
+package data;
+
+/**
+ * Created By: Naman Agarwal
+ * User ID: naman2807
+ * Package Name: data
+ * Project Name: BankingApplication
+ * Date: 22-05-2021
+ */
+
+public class Bank {
+}
