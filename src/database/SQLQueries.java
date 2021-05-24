96daaa1d83c2bfe20ac63fa35b6b417890b50722
naman2807 <namanagarwaldbs438@gmail.com>
2021-05-24 11:30:59 +0530

    Added a new constant string of loan table.

--- src/database/SQLQueries.java
+++ src/database/SQLQueries.java
@@ -23,9 +23,11 @@ public class SQLQueries {
     private static final String TRANSACTIONS_ID = "transactionID";
     private static final String TRANSACTION_TIME = "transaction_time";
     private static final String EMPLOYEE_TABLE = "employee_table";
+    private static final String LOAN_TABLE = "loan_table";
     private static final String EMPLOYEE_ID = "userid";
     private static final String CUSTOMER_NAME = "name";
     private static final String CUSTOMER_AGE = "age";
+
     private static final String CUSTOMER_ADDRESS = "address";
     private static final String CUSTOMER_PARENT_NAME = "parentname";
     private static final String CUSTOMER_PHONE_NUMBER = "phone_number";
