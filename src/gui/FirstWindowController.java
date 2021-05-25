2bd66cec1fc1d3dba6744a3b0fec904e85a39964
naman2807 <namanagarwaldbs438@gmail.com>
2021-05-25 10:18:18 +0530

    Extracted the text from name private field.

--- src/gui/FirstWindowController.java
+++ src/gui/FirstWindowController.java
@@ -57,7 +57,7 @@ public class FirstWindowController {
                         "password again.\nThank You!");
             }
         }else if(event.getSource() == addCustomer){
-
+            String cusName = name.getText();
         }
     }
 
