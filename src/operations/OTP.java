9ac50978887f349db48a9321229c1f12560f786d
naman2807 <namanagarwaldbs438@gmail.com>
2021-05-26 09:58:01 +0530

    Created a new OTP.java class

---
+++ src/operations/OTP.java
@@ -0,0 +1,12 @@
+package operations;
+
+/**
+ * Created By: Naman Agarwal
+ * User ID: naman2807
+ * Package Name: operations
+ * Project Name: BankingApplication
+ * Date: 26-05-2021
+ */
+
+public class OTP {
+}
