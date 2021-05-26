2bd2d824fa3df38feaa57d3b5dbac95420febc20
naman2807 <namanagarwaldbs438@gmail.com>
2021-05-26 12:59:09 +0530

    Implemented the sendOTPToLinkedAccountNumber() in OTP.java

--- src/operations/OTP.java
+++ src/operations/OTP.java
@@ -1,5 +1,9 @@
 package operations;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Random;
 
 /**
@@ -28,6 +32,30 @@ public final class OTP {
     }
 
     public static void sendOTPToLinkedAccountNumber(){
+        try {
+            // Construct data
+            String apiKey = "apikey=" + "MGJkMTc3NWQxZTBlNDYwMjk4OWJlNzhlZTJhYzI5ZGI=";
+            String message = "&message=" + "This is your message";
+            String sender = "&sender=" + "Bank Of India";
+            String numbers = "&numbers=" + "919838164810";
 
+            // Send data
+            HttpURLConnection conn = (HttpURLConnection) new URL("https://api.textlocal.in/send/?").openConnection();
+            String data = apiKey + numbers + message + sender;
+            conn.setDoOutput(true);
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Length", Integer.toString(data.length()));
+            conn.getOutputStream().write(data.getBytes("UTF-8"));
+//            final BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+//            final StringBuffer stringBuffer = new StringBuffer();
+//            String line;
+//            while ((line = rd.readLine()) != null) {
+//                stringBuffer.append(line);
+//            }
+//            rd.close();
+        } catch (Exception e) {
+            System.out.println("Error SMS "+e);
+
+        }
     }
 }
