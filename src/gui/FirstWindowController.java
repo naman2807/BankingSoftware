8142d8de7b5b170f2dd60399d896eed859934d76
naman2807 <namanagarwaldbs438@gmail.com>
2021-05-27 12:43:12 +0530

    Checked if Action Event raised is to show transaction record.

--- src/gui/FirstWindowController.java
+++ src/gui/FirstWindowController.java
@@ -108,6 +108,9 @@ public class FirstWindowController {
 
         }else if(event.getSource() == doTransaction){
             doTransaction(getSelectedToggleButton());
+
+        }else if(event.getSource() == showTransactionPane){
+
         }
     }
 
