7ead5f4fe7945231a0d0a49309e2ec7e1464aed2
naman2807 <namanagarwaldbs438@gmail.com>
2021-05-28 12:31:47 +0530

    Added a new string LOAN_TYPE in SQLQueries.java

--- src/database/SQLQueries.java
+++ src/database/SQLQueries.java
@@ -24,11 +24,12 @@ public class SQLQueries {
     private static final String TRANSACTION_TIME = "time";
     private static final String EMPLOYEE_TABLE = "employee_table";
     private static final String LOAN_TABLE = "loan_table";
+    private static final String LOAN_AMOUNT = "loan_amount";
+    private static final String LOAN_TYPE = "loan_type";
     private static final String EMPLOYEE_ID = "userid";
     private static final String OPERATION = "operation_type";
     private static final String CUSTOMER_NAME = "name";
     private static final String CUSTOMER_AGE = "age";
-    private static final String LOAN_AMOUNT = "loan_amount";
     private static final String CUSTOMER_ADDRESS = "address";
     private static final String CUSTOMER_PARENT_NAME = "parentname";
     private static final String CUSTOMER_PHONE_NUMBER = "phone_number";
