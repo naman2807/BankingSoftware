2debbdafca0081bad75f79b3f53ba4c59a759931
naman2807 <namanagarwaldbs438@gmail.com>
2021-05-28 13:07:42 +0530

    Generated getters and setters of all fields in Loan.java

--- src/data/Loan.java
+++ src/data/Loan.java
@@ -29,4 +29,52 @@ public class Loan {
         this.loanType.set(loanType);
         this.dueDate.set(dueDate);
     }
+
+    public String getAccountNumber() {
+        return accountNumber.get();
+    }
+
+    public SimpleStringProperty accountNumberProperty() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber.set(accountNumber);
+    }
+
+    public double getLoanAmount() {
+        return loanAmount.get();
+    }
+
+    public SimpleDoubleProperty loanAmountProperty() {
+        return loanAmount;
+    }
+
+    public void setLoanAmount(double loanAmount) {
+        this.loanAmount.set(loanAmount);
+    }
+
+    public String getLoanType() {
+        return loanType.get();
+    }
+
+    public SimpleStringProperty loanTypeProperty() {
+        return loanType;
+    }
+
+    public void setLoanType(String loanType) {
+        this.loanType.set(loanType);
+    }
+
+    public Date getDueDate() {
+        return dueDate.get();
+    }
+
+    public SimpleObjectProperty<Date> dueDateProperty() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate.set(dueDate);
+    }
 }
